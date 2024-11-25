7cc749f7f188907dcc9a2a68ad7ee14940d5c3fe
Jose Annunziato <jannunzi@gmail.com>
2024-11-25 17:48:52 -0500

    a6

---
+++ Kanbas/Courses/routes.js
@@ -0,0 +1,7 @@
+import * as dao from "./dao.js";
+export default function CourseRoutes(app) {
+  app.get("/api/courses", async (req, res) => {
+    const courses = await dao.findAllCourses();
+    res.send(courses);
+  });
+}
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // const express = require("express");
+import mongoose from "mongoose";
+import "dotenv/config";
+import session from "express-session";
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5/index.js";
@@ -6,10 +9,35 @@ import PathParameters from "./Lab5/PathParameters.js";
 import QueryParameters from "./Lab5/QueryParameters.js";
 import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
 import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 
+const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas-02-fa24";
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
 
 Hello(app);
 Lab5(app);
@@ -17,5 +45,7 @@ PathParameters(app);
 QueryParameters(app);
 WorkingWithObjects(app);
 WorkingWithArrays(app);
+UserRoutes(app);
+CourseRoutes(app);
 
 app.listen(process.env.PORT || 4000);
